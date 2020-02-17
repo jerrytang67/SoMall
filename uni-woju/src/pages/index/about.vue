21947eb4cce538c25b989b2f09390dd89ac315b0
JerryTang <tt-tjw@qq.com>
2020-02-17 22:09:52 +0800

    add uni-woju

---
+++ uni-woju/src/pages/index/about.vue
@@ -0,0 +1,24 @@
+
+<template>
+   <view>
+      Hello {{name}}
+   </view>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Inject, Watch, Ref } from "vue-property-decorator";
+
+@Component({})
+export default class About extends Vue {
+   get name() {
+      return "TT";
+   }
+
+   created() {
+      console.log("created");
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ uni-woju/src/pages/index/my.vue
@@ -0,0 +1,30 @@
+
+<template>
+   <view>
+      Hello {{name}}
+      <view>
+         <van-button type="default">默认按钮</van-button>
+         <van-button type="primary">主要按钮</van-button>
+         <van-button type="info">信息按钮</van-button>
+         <van-button type="warning">警告按钮</van-button>
+         <van-button type="danger">危险按钮</van-button>
+      </view>
+   </view>
+</template>
+<script lang="ts">
+import { Component, Vue, Inject, Watch, Ref } from "vue-property-decorator";
+
+@Component({})
+export default class About extends Vue {
+   get name() {
+      return "TT";
+   }
+
+   created() {
+      console.log("created");
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
