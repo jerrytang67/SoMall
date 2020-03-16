745b25e47a4c658bd1ddb0370171e14940221a7e
JerryTang <tt-tjw@qq.com>
2020-03-17 01:31:42 +0800

    docker打包

--- backEnd/modules/TT.Abp.VisitorManagement/TT/Abp/VisitorManagement/Domain/CredentialType.cs
+++ backEnd/modules/TT.Abp.VisitorManagement/TT/Abp/VisitorManagement/Domain/CredentialType.cs
@@ -14,7 +14,10 @@
         {
             Input = 0,
             TextArea = 1,
-            Radio = 2
+            Radio = 2,
+            Checkbox = 3,
+            Image = 6,
+            File = 7
         }
 
         public enum FormTheme
