7011424b8878a5111365c26784a2ddc70f3b39eb
JerryTang <tt-tjw@qq.com>
2020-03-27 17:06:49 +0800

    db update

--- backEnd/src/TT.SoMall.EntityFrameworkCore.DbMigrations/Migrations/20200327090630_db_module_mall_update.Designer.cs
+++ backEnd/src/TT.SoMall.EntityFrameworkCore.DbMigrations/Migrations/20200327090630_db_module_mall_update.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TT.SoMall.EntityFrameworkCore;
 
 namespace TT.SoMall.Migrations
 {
     [DbContext(typeof(SoMallMigrationsDbContext))]
-    partial class SoMallMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200327090630_db_module_mall_update")]
+    partial class db_module_mall_update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
