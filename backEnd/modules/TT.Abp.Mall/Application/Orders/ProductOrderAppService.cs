7ff068d9fdde4fe93f802ca02f26a3823704475e
TT <tt-tjw@qq.com>
2020-04-14 15:24:29 +0800

    make vuex prop private

---
+++ backEnd/modules/TT.Abp.Mall/Application/Orders/ProductOrderAppService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using TT.Abp.Mall.Domain.Orders;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using TT.Abp.Mall.Domain.Products;
+using Volo.Abp.Domain.Repositories;
+
+namespace TT.Abp.Mall.Application.Orders
+{
+    public interface IProductOrderAppService : ICrudAppService<ProductOrderDto, Guid, PagedAndSortedResultRequestDto, ProductOrderCreateOrUpdateDto, ProductOrderCreateOrUpdateDto>
+    {
+    }
+
+    public class ProductOrderAppService :
+        CrudAppService<ProductOrder, ProductOrderDto, Guid, PagedAndSortedResultRequestDto, ProductOrderCreateOrUpdateDto, ProductOrderCreateOrUpdateDto>,
+        IProductOrderAppService
+    {
+        public ProductOrderAppService(IRepository<ProductOrder, Guid> repository) : base(repository)
+        {
+        }
+
+        public override async Task<ProductOrderDto> GetAsync(Guid id)
+        {
+            await CheckGetPolicyAsync();
+
+            var entity = await GetEntityByIdAsync(id);
+            return MapToGetOutputDto(entity);
+
+
+            return base.GetAsync(id);
+        }
+
+        public override Task<ProductOrderDto> CreateAsync(ProductOrderCreateOrUpdateDto input)
+        {
+            throw new Exception("not use");
+        }
+    }
+
+
+    public class ProductOrderDto : EntityDto<Guid>
+    {
+    }
+
+    public class ProductOrderCreateOrUpdateDto
+    {
+    }
+}
