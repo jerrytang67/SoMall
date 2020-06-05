aab2beb2efa055c9b9fc1f76af0f084c5dc8fba2
TT <tt-tjw@qq.com>
2020-06-05 23:33:31 +0800

    db migrations

--- backEnd/src/TT.SoMall.EntityFrameworkCore.DbMigrations/Migrations/20200605153030_abp_2.9.Designer.cs
+++ backEnd/src/TT.SoMall.EntityFrameworkCore.DbMigrations/Migrations/20200605153030_abp_2.9.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TT.SoMall.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -10,9 +11,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace TT.SoMall.Migrations
 {
     [DbContext(typeof(SoMallMigrationsDbContext))]
-    partial class SoMallMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200605153030_abp_2.9")]
+    partial class abp_29
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
