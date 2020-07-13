17df8dfab14fe1c01dc5a49cf7d2088650430e91
TT <tt-tjw@qq.com>
2020-07-13 10:28:43 +0800

    ng-zorro update to 9.3,fixed upload previewimg bugs

--- angular/src/app/shared/components/tt-upload/tt-upload.component.ts
+++ angular/src/app/shared/components/tt-upload/tt-upload.component.ts
@@ -17,7 +17,7 @@ import { OssService } from 'src/store/oss/oss.service';
 <div class="clearfix">
     <nz-upload #host nzListType="picture-card" [(nzFileList)]="imgList" [customSuffix]="'!'"
     [nzAction]="ossQuery.uploadUrl$ | async" [nzCustomRequest]="customReq" (nzChange)="handleChange($event)" [nzRemove]="handleRemove"
-        [nzShowButton]="imgList.length < length" [nzShowUploadList]="showUploadList" [nzPreview]="handlePreview" >
+        [nzShowButton]="imgList.length < length" [nzShowUploadList]="showUploadList" [nzPreview]="handlePreview" [nzPreviewIsImage]="previewIsImage" >
         <i nz-icon nzType="plus"></i>
         <div class="ant-upload-text">{{ "upload" | translate}}</div>
     </nz-upload>
@@ -129,6 +129,10 @@ export class TtUploadComponent implements OnChanges {
         this.previewVisible = true;
         console.log(this.imgList)
     };
+
+    previewIsImage = (file: UploadFile) => {
+        return true;
+    }
 
 
     customReq = (item: UploadXHRArgs) => {
