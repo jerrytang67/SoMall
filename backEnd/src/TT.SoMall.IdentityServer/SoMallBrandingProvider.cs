0b08b6987b6750c6a3ed1a1403035715bf8d6f0b
TT <tt-tjw@qq.com>
2021-04-04 22:08:01 +0800

    modify api rewrite

---
+++ backEnd/src/TT.SoMall.IdentityServer/SoMallBrandingProvider.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Ui.Branding;
+using Volo.Abp.DependencyInjection;
+
+namespace TT.SoMall
+{
+    [Dependency(ReplaceServices = true)]
+    public class SoMallBrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName => "SoMall";
+    }
+}
